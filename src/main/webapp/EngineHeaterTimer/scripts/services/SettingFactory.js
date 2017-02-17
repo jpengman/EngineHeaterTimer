adae476b76a8cbc5eb24e5c27bd0e5e6e8305bc4
jesperengman <jesperengman@nuc>
2017-02-17 09:08:49 +0100

    removed duplicates

--- src/main/webapp/EngineHeaterTimer/scripts/services/SettingFactory.js
+++
@@ -1,4 +0,0 @@
-angular.module('engineHeaterTimer').factory('SettingResource', function($resource){
-    var resource = $resource('../rest/settings/:SettingId',{SettingId:'@settingId'},{'queryAll':{method:'GET',isArray:true},'query':{method:'GET',isArray:false},'update':{method:'PUT'}});
-    return resource;
-});
